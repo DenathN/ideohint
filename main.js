eb2dc7b41efb04760ffafa60c12969be2fe234a5
be5invis <belleve@typeof.net>
2016-09-17 11:24:14 +0800

    r0.7.0

---
+++ main.js
@@ -0,0 +1,4 @@
+exports.findstem = require('./findstem');
+exports.extractfeature = require('./extractfeature');
+exports.hinter = require('./hinter');
+exports.instructor = require('./instructor');
