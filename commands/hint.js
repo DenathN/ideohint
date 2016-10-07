5f4009c2b6e855c0d3ab384a6741f8f304ba66f5
be5invis <belleve@typeof.net>
2016-10-07 08:51:55 +0800

    added VTT exporter

--- commands/hint.js
+++ commands/hint.js
@@ -1,38 +1,38 @@
-"use strict"
+"use strict";
 
-var fs = require('fs');
-var readline = require('readline');
-var stream = require('stream');
-var util = require('util');
-var devnull = require('dev-null');
-var hint = require('../hinter').hint;
-var paramfileLib = require('../paramfile');
-var strategyLib = require('../strategy');
+var fs = require("fs");
+var readline = require("readline");
+var stream = require("stream");
+var util = require("util");
+var devnull = require("dev-null");
+var hint = require("../hinter").hint;
+var paramfileLib = require("../paramfile");
+var strategyLib = require("../strategy");
 
-exports.command = 'hint';
-exports.describe = 'Hint a feature file (hgf).'
+exports.command = "hint";
+exports.describe = "Hint a feature file (hgf).";
 exports.builder = function (yargs) {
-	return yargs.alias('o', 'output-into')
-		.alias('?', 'help')
-		.alias('p', 'parameters')
-		.describe('help', 'Displays this help.')
-		.describe('o', 'Output sfd path. When absent, the result sfd is written to STDOUT.')
-		.describe('d', 'Only process dk+m\'th glyphs in the feature file. Combine with -m for parallel processing.')
-		.describe('m', 'Only process dk+m\'th glyphs in the feature file. Combine with -d for parallel processing.')
-		.describe('parameters', 'Specify parameter file (in TOML).');
-}
+	return yargs.alias("o", "output-into")
+		.alias("?", "help")
+		.alias("p", "parameters")
+		.describe("help", "Displays this help.")
+		.describe("o", "Output sfd path. When absent, the result sfd is written to STDOUT.")
+		.describe("d", "Only process dk+m'th glyphs in the feature file. Combine with -m for parallel processing.")
+		.describe("m", "Only process dk+m'th glyphs in the feature file. Combine with -d for parallel processing.")
+		.describe("parameters", "Specify parameter file (in TOML).");
+};
 
-function by_rp(a, b) { return a[0] - b[0] || a[1] - b[1] }
-function getIpsaCalls(glyph) {
+function by_rp (a, b) { return a[0] - b[0] || a[1] - b[1];}
+function getIpsaCalls (glyph) {
 	var ip = [];
 	var sa = [];
 	for (var j = 0; j < glyph.interpolations.length; j++) {
 		if (!ip[glyph.interpolations[j][3]]) ip[glyph.interpolations[j][3]] = [];
-		ip[glyph.interpolations[j][3]].push(glyph.interpolations[j])
+		ip[glyph.interpolations[j][3]].push(glyph.interpolations[j]);
 	}
 	for (var j = 0; j < glyph.shortAbsorptions.length; j++) {
 		if (!sa[glyph.shortAbsorptions[j][2]]) sa[glyph.shortAbsorptions[j][2]] = [];
-		sa[glyph.shortAbsorptions[j][2]].push(glyph.shortAbsorptions[j])
+		sa[glyph.shortAbsorptions[j][2]].push(glyph.shortAbsorptions[j]);
 	}
 	var ipsacalls = [];
 	var maxpri = Math.max(ip.length - 1, sa.length - 1);
@@ -43,14 +43,13 @@ function getIpsaCalls(glyph) {
 	}
 	return ipsacalls;
 }
-function slicelast(x) { return x.slice(0, -1); }
+function slicelast (x) { return x.slice(0, -1); }
 
 exports.handler = function (argv) {
-
-	if (argv.help) { yargs.showHelp(); process.exit(0) }
+	if (argv.help) { yargs.showHelp(); process.exit(0); }
 
 	var inStream = argv._[1] ? fs.createReadStream(argv._[1]) : process.stdin;
-	var outStream = argv.o ? fs.createWriteStream(argv.o, { encoding: 'utf-8' }) : process.stdout;
+	var outStream = argv.o ? fs.createWriteStream(argv.o, { encoding: "utf-8" }) : process.stdout;
 	var rl = readline.createInterface(inStream, devnull());
 
 
@@ -60,15 +59,15 @@ exports.handler = function (argv) {
 	var divide = argv.d || 1;
 	var modulo = argv.m || 0;
 
-	function pad(s, p, n) {
-		s = '' + s;
+	function pad (s, p, n) {
+		s = "" + s;
 		while (s.length < n) s = p + s;
 		return s;
 	}
-	function progressbar(u, len) {
-		var buf = '';
+	function progressbar (u, len) {
+		var buf = "";
 		for (var j = 1; j <= len; j++) {
-			buf += (j > u * len) ? ' ' : '#'
+			buf += (j > u * len) ? " " : "#";
 		}
 		return buf;
 	}
@@ -77,15 +76,15 @@ exports.handler = function (argv) {
 	var pendings = [];
 	var PROGRESS_LENGTH = 30;
 
-	function showProgressBar(currentProgress, j, n) {
+	function showProgressBar (currentProgress, j, n) {
 		var pb = progressbar(j / n, PROGRESS_LENGTH);
 		if (pb !== currentProgress) {
-			process.stderr.write('HGFHINT: Hinting [' + pb + '](#' + pad(j, ' ', 5) + '/' + pad(n, ' ', 5) + ')' + ' of ' + (argv._[1] || '(stdin)') + ' ' + pad(modulo, '0', 3) + "d" + pad(divide, '0', 3) + '\n');
-		};
+			process.stderr.write("HGFHINT: Hinting [" + pb + "](#" + pad(j, " ", 5) + "/" + pad(n, " ", 5) + ")" + " of " + (argv._[1] || "(stdin)") + " " + pad(modulo, "0", 3) + "d" + pad(divide, "0", 3) + "\n");
+		}
 		return pb;
 	}
 
-	function finish() {
+	function finish () {
 		if (finished) return;
 		finished = true;
 		var currentProgress = progressbar(0, PROGRESS_LENGTH);
@@ -93,20 +92,11 @@ exports.handler = function (argv) {
 			var data = pendings[glyphIndex];
 			var glyph = data[2];
 			var stemActions = [];
-			var nMDRPnr = 0, nMDRPr = 0;
 			for (var ppem = strategy.PPEM_MIN; ppem <= strategy.PPEM_MAX; ppem++) {
 				var actions = hint(glyph, ppem, strategy);
-				for (var k = 0; k < actions.length; k++) {
-					if (actions[k].length === 4) {
-						nMDRPnr += 1
-					} else if (Math.round(actions[k][3]) === actions[k][4] && Math.abs(actions[k][3] - actions[k][4]) < 0.48) {
-						nMDRPr += 1
-					}
-				}
 				stemActions[ppem] = actions;
 			}
 			currentProgress = showProgressBar(currentProgress, glyphIndex, pendings.length);
-
 
 			var sideIndependent = {
 				bottomBluePoints: glyph.bottomBluePoints,
@@ -121,24 +111,23 @@ exports.handler = function (argv) {
 						advKey: s.advKey.id,
 						posAlign: s.posAlign,
 						advAlign: s.advAlign
-					}
+					};
 				})
 			};
 			var recordLine = [data[0], data[1], { si: sideIndependent, sd: stemActions }];
-			outStream.write(JSON.stringify(recordLine) + '\n');
-		};
+			outStream.write(JSON.stringify(recordLine) + "\n");
+		}
 		currentProgress = showProgressBar(currentProgress, glyphIndex, pendings.length);
 		if (process.stdout !== outStream) outStream.end();
 	}
 
 	var j = 0;
-	rl.on('line', function (line) {
+	rl.on("line", function (line) {
 		if (j % divide === modulo % divide) {
 			var l = line.trim();
 			if (l) pendings.push(JSON.parse(l));
 		}
 		j += 1;
 	});
-	rl.on('close', finish);
-
-}
+	rl.on("close", finish);
+};
