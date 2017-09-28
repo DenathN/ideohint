f2e68ef451e50b343baa6aa167e48b162af09694
be5invis <belleve@typeof.net>
2017-09-28 20:32:30 +0800

    refine progress bar

--- commands/support/progress.js
+++ commands/support/progress.js
@@ -9,24 +9,35 @@ function pad(s, p, n) {
 function progressbar(u, len) {
 	var buf = "";
 	for (var j = 1; j <= len; j++) {
-		buf += (j > u * len) ? " " : "#";
+		buf += j > u * len ? " " : "#";
 	}
 	return buf;
 }
 
-function showProgressBar(name, currentProgress, j, n) {
-	var pb = progressbar(j / n, PROGRESS_LENGTH);
-	if (pb !== currentProgress) {
-		process.stderr.write("[" + pb + "](#" + pad(j, " ", 5) + "/" + pad(n, " ", 5) + ")" + " of " + name + "\n");
+function showProgressBar(name, currentProgress, j, n, lastUpdateTime) {
+	const pb = progressbar(j / n, PROGRESS_LENGTH);
+	const now = new Date();
+	if (j === n || (pb !== currentProgress && now - lastUpdateTime > 2048)) {
+		process.stderr.write(
+			"[" + pb + "](#" + pad(j, " ", 5) + "/" + pad(n, " ", 5) + ")" + " of " + name + "\n"
+		);
+		lastUpdateTime = now;
 	}
-	return pb;
+	return [pb, lastUpdateTime];
 }
 
 exports.progress = function progressForEach(name, items, fn) {
 	let currentProgress = progressbar(0, PROGRESS_LENGTH);
+	let lastUpdateTime = new Date();
 	for (let j = 0; j < items.length; j++) {
 		fn(items[j], j);
-		currentProgress = showProgressBar(name, currentProgress, j, items.length);
+		[currentProgress, lastUpdateTime] = showProgressBar(
+			name,
+			currentProgress,
+			j,
+			items.length,
+			lastUpdateTime
+		);
 	}
 	currentProgress = showProgressBar(name, currentProgress, items.length, items.length);
-}
+};
