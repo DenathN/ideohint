b39b5e21d6c5d596a741eea2aad844856b892c83
belleve <belleve@typeof.net>
2018-04-16 18:46:45 +0800

    bump ver

--- core/index.js
+++ core/index.js
@@ -7,7 +7,7 @@ const { parseOTD } = require("./otdParser");
 const { xclamp, toVQ } = require("../support/common");
 const roundings = require("../support/roundings");
 
-exports.version = 1016000;
+exports.version = 1016002;
 
 exports.hintSingleGlyph = function(contours, strategy) {
 	return exports.decideHints(
